ce9979a903e4e1a01b5fc2bf9437c7f4ab951b43
AbidulCrazyBoy <errornik12@gmail.com>
2025-05-09 11:02:59 +0200

    Add files via upload
    Skrypt js mihail dawaj rabotaju

---
+++ skrypt.js
@@ -0,0 +1,49 @@
+let odpowiedzi = [
+    "Świetnie!",
+    "Kto gra główną rolę?",
+    "Lubisz filmy Tego reżysera?",
+    "Będę 10 minut wcześniej",
+    "Może kupimy sobie popcorn?",
+    "Ja wolę Colę",
+    "Zaproszę jeszcze Grześka",
+    "Tydzień temu też byłem w kinie na Diunie",
+    "Ja funduję bilety"
+];
+
+let x = Math.floor(Math.random() * 9);
+
+function wyslijwiadomosc() {
+    let creating = document.createElement('div');
+    creating.classList.add('czatwiadomosc');
+
+    let image = document.createElement('img');
+    image.src = 'Jolka.jpg';
+    image.alt = 'Jolanta Nowak';
+
+    let inputValue = document.getElementById("wiadomoscinput").value
+    let text = document.createElement('p')
+    text.textContent = inputValue
+
+    creating.appendChild(image);
+    creating.appendChild(text)
+
+    document.querySelector('.oknoczat').appendChild(creating);
+}
+
+function generujlosowaodpowiedz() {
+    let creating = document.createElement('div');
+    creating.classList.add('czatwiadomosc');
+
+    let image = document.createElement('img');
+    image.src = 'Krzysiek.jpg';
+    image.alt = 'Krzysztof Łukasiński';
+
+    let inputValue = odpowiedzi[x]
+    let text = document.createElement('p')
+    text.textContent = inputValue
+
+    creating.appendChild(image);
+    creating.appendChild(text)
+
+    document.querySelector('.oknoczat').appendChild(creating);
+}
