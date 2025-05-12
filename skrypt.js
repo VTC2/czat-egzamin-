f1e5edeb7e1517d479095adaaf4f5fa5dee3a9d7
AbidulCrazyBoy <errornik12@gmail.com>
2025-05-12 10:40:26 +0200

    Add files via upload
    fixed

--- skrypt.js
+++ skrypt.js
@@ -10,7 +10,6 @@ let odpowiedzi = [
     "Ja funduję bilety"
 ];
 
-
 function wyslijwiadomosc() {
     let creating = document.createElement('div');
     creating.classList.add('czatwiadomosc');
@@ -18,33 +17,39 @@ function wyslijwiadomosc() {
     let image = document.createElement('img');
     image.src = 'Jolka.jpg';
     image.alt = 'Jolanta Nowak';
+    image.style.marginRight = '10px';
 
-    let inputValue = document.getElementById("wiadomoscinput").value
-    let text = document.createElement('p')
-    text.textContent = inputValue
+    let inputValue = document.getElementById("wiadomoscinput").value;
+    let text = document.createElement('p');
+    text.textContent = inputValue;
 
     creating.appendChild(image);
     creating.appendChild(text);
+
+    creating.style.justifyContent = 'flex-start';
 
     document.querySelector('.oknoczat').appendChild(creating);
 }
 
 function generujlosowaodpowiedz() {
-    let x = Math.floor(Math.random() * 9);
+    let x = Math.floor(Math.random() * odpowiedzi.length);
 
     let creating = document.createElement('div');
     creating.classList.add('czatwiadomosc');
 
-    let inputValue = odpowiedzi[x]
-    let text = document.createElement('p')
-    text.textContent = inputValue
+    let inputValue = odpowiedzi[x];
+    let text = document.createElement('p');
+    text.textContent = inputValue;
 
     let image = document.createElement('img');
     image.src = 'Krzysiek.jpg';
     image.alt = 'Krzysztof Łukasiński';
+    image.style.marginLeft = '10px';
 
     creating.appendChild(text);
     creating.appendChild(image);
-    
+
+    creating.style.justifyContent = 'flex-end';
+
     document.querySelector('.oknoczat').appendChild(creating);
 }
